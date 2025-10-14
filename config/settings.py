dfa8487ac7de2b601b61bd60b3ab79d19601b09b
idcard3-beep <idcard3@gmail.com>
2025-10-14 23:00:03 +0900

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -8,14 +8,16 @@ MAX_FILE_MB = int(os.getenv("MAX_FILE_MB", "10"))
 
 VIEW_TOKEN_SECRET = os.getenv("VIEW_TOKEN_SECRET", "change-me")
 
-# PostgreSQL (기본 데이터베이스)
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+# PostgreSQL 설정 (운영 서버)
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "dpg-d3mctk0gjchc73d1t57g-a")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "secretboard")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "rich")
-# Render.com에서는 DATABASE_URL 환경변수로 제공됨
-DATABASE_URL = os.getenv("DATABASE_URL", None)
+POSTGRES_USER = os.getenv("POSTGRES_USER", "secretboard_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "A7kKvXyBD9IYMi4zHhbNatctDNE5gYlA")
+
+# Render.com PostgreSQL DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL", 
+    "postgresql://secretboard_user:A7kKvXyBD9IYMi4zHhbNatctDNE5gYlA@dpg-d3mctk0gjchc73d1t57g-a/secretboard")
 
 # MSxSQL (로컬 개발용 - 필요시에만 사용)
 ##MSxSQL_DRIVER = os.getenv("MSxSQL_DRIVER", "SQL Server")
